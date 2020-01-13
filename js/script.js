9d5613abe2d222396ffd3cdb5eac877b72d52631
Taya Kitt <taya.kitt@gmail.com>
2020-01-13 14:28:19 -0500

    add db connection

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+
+// Initialize firebase
+firebase.initializeApp(FIREBASECONFIG);
+var db = firebase.firestore();
+
+db.collection("ClinicCollection").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+  });
+});
+
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGF5YWtpdHQiLCJhIjoiY2s1Y284Ym9hMW50ODNnbzMwbHBmaHR1eiJ9._DGyRf1U27iZwnGv5Ar56A', {
