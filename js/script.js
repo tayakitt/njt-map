107effcbbfb48ce5abfbcdfb244c7338649707be
Taya Kitt <taya.kitt@gmail.com>
2020-01-13 15:28:24 -0500

    use google maps api

--- js/script.js
+++ js/script.js
@@ -5,15 +5,38 @@ var db = firebase.firestore();
 
 db.collection("ClinicCollection").get().then((querySnapshot) => {
   querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data()}`);
+    // console.log(`${doc.id} => ${doc.data()}`);
   });
 });
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGF5YWtpdHQiLCJhIjoiY2s1Y284Ym9hMW50ODNnbzMwbHBmaHR1eiJ9._DGyRf1U27iZwnGv5Ar56A', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox/streets-v11',
-  accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+var map;
+
+function initMap() {
+  geocoder = new google.maps.Geocoder();
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: { lat: -28, lng: 137 }
+  });
+  map.data.loadGeoJson('https://storage.googleapis.com/mapsdevsite/json/google.json');
+  // codeAddress("Bangkok, Thailand");
+};
+
+function codeAddress(address) {
+  geocoder.geocode({ address: address }, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      //center the map over the result
+      map.setCenter(results[0].geometry.location);
+
+      //place a marker at the location
+      var marker = new google.maps.Marker(
+        {
+          map: map,
+          position: results[0].geometry.location
+        });
+      console.log({ results });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+};
