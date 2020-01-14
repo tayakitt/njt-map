b776db084b8cd9ecc2b7a55dc2e1f2ca964e36a5
Taya Kitt <taya.kitt@gmail.com>
2020-01-13 20:54:14 -0500

    database config

---
+++ js/config.js
@@ -0,0 +1,6 @@
+var FIREBASECONFIG = {
+  apiKey: "AIzaSyBB6JyfIq0QO-W3JE6mqaHJoi8CQdBX1Ew",
+  authDomain: "njt-notjusttourists.firebaseapp.com",
+  databaseURL: "https://njt-notjusttourists.firebaseio.com",
+  projectId: "njt-notjusttourists",
+};
