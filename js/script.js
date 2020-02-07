603f9a421fb29e2bd674c6ace570eb39dec874a2
Taya Kitt <taya.kitt@gmail.com>
2020-02-06 19:02:59 -0500

    add plugin for marker cluster

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ firebase.initializeApp(FIREBASECONFIG);
 var db = firebase.firestore();
 
 // get trip and update GeoJSON data
-getTrips();
+// getTrips();
 
 // create map and set center lat & lng
 var map = L.map('map').setView([21.0079, 10.9408], 3);
@@ -15,6 +15,57 @@ L.tileLayer.provider('MapBox', {
   id: 'mapbox.light',
   accessToken: 'pk.eyJ1IjoidGF5YWtpdHQiLCJhIjoiY2s1Y284Ym9hMW50ODNnbzMwbHBmaHR1eiJ9._DGyRf1U27iZwnGv5Ar56A'
 }).addTo(map);
+
+createMarkers();
+
+// create markers
+function createMarkers() {
+  var tripInfo = {};
+  var markersInfo = [];
+  db.collection("TripCollection").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      var data = doc.data();
+      var country = data['the_country_i_am_traveling_to'];
+      var weight = data["weight_lbs"] ? parseInt(data["weight_lbs"]) : 0;
+      var suitcases = data["number_suitcases"] ? parseInt(data["number_suitcases"]) : 0;
+      var existingCountry = tripInfo[country];
+
+      // aggregate weight, suitcases, and trips for each country
+      if (existingCountry) {
+        existingCountry["weight"] += weight;
+        existingCountry["suitcases"] += suitcases;
+        existingCountry["trips"] += 1;
+      } else if (country) { //ignore trips where country is not provided
+        tripInfo[country] = { weight, suitcases, trips: 1 };
+      }
+    });
+
+    // add information to GeoJSON properties
+    countryCoords.forEach(country => {
+      const countryName = country["country"];
+
+      // add information if trip information exist for this country
+      if (tripInfo[countryName]) {
+        markersInfo.push({ ...tripInfo[countryName], ...country });
+      }
+    });
+
+    // cluster markers
+    var markers = L.markerClusterGroup();
+
+    for (var i = 0; i < markersInfo.length; i++) {
+      var markerInfo = markersInfo[i];
+      var title = markerInfo["countryName"];
+      var marker = L.marker(new L.LatLng(markerInfo["latitude"], markerInfo["longitude"]), { title: title });
+      marker.bindPopup(title);
+      markers.addLayer(marker);
+    }
+
+    map.addLayer(markers);
+
+    console.log(markersInfo);
+  });
+}
 
 function getTrips() {
   var tripInfo = {};
