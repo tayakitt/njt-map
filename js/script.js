248c529edbbdbbf3de31f01f525a42b715fd3e1b
Taya Kittananthawongs <38886591+tayakitt@users.noreply.github.com>
2020-02-06 19:37:16 -0500

    Use leaflet clustering (#3)
    * add plugin for marker cluster
    
    * use custom markers to display num of suitcases
    
    * info box
    
    * clean up
    
    * fix tooltip description

--- js/script.js
+++ js/script.js
@@ -3,12 +3,8 @@
 firebase.initializeApp(FIREBASECONFIG);
 var db = firebase.firestore();
 
-// get trip and update GeoJSON data
-getTrips();
-
 // create map and set center lat & lng
-var map = L.map('map').setView([21.0079, 10.9408], 3);
-var geojson;
+var map = L.map('map').setView([21.0079, 10.9408], 2);
 
 // use mapBox as the base map provider
 L.tileLayer.provider('MapBox', {
@@ -16,8 +12,12 @@ L.tileLayer.provider('MapBox', {
   accessToken: 'pk.eyJ1IjoidGF5YWtpdHQiLCJhIjoiY2s1Y284Ym9hMW50ODNnbzMwbHBmaHR1eiJ9._DGyRf1U27iZwnGv5Ar56A'
 }).addTo(map);
 
-function getTrips() {
+// create markers layer
+createMarkers();
+
+function createMarkers() {
   var tripInfo = {};
+  var markersInfo = [];
   db.collection("TripCollection").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
       var data = doc.data();
@@ -36,151 +36,55 @@ function getTrips() {
       }
     });
 
-    // add information to GeoJSON properties
-    countries["features"].forEach(feature => {
-      var featureCountry = feature["properties"]["country"];
+    // combine tripInfo with markersInfo
+    countryCoords.forEach(country => {
+      const countryName = country["country"];
 
-      // add information if trip information exist for this country
-      if (tripInfo[featureCountry]) {
-        feature["properties"] = { ...feature["properties"], ...tripInfo[featureCountry] }
+      // only mark countries with suitcases > 0
+      if (tripInfo[countryName] && tripInfo[countryName]["suitcases"] > 0) {
+        markersInfo.push({ ...tripInfo[countryName], ...country });
       }
     });
 
-    geojson = L.geoJson(countries, { style: style, onEachFeature: onEachFeature }).addTo(map);
-    console.log({ tripInfo });
-    console.log("done updating geoJSON");
-    console.log({ countries });
+    // initialize marker cluster group
+    var markers = L.markerClusterGroup({
+      iconCreateFunction: function (cluster) {
+        var markers = cluster.getAllChildMarkers();
+        var clusterSuitcases = 0;
+
+        // count suitcases in the cluster
+        for (var i = 0; i < markers.length; i++) {
+          clusterSuitcases += markers[i]["options"]["suitcases"];
+        }
+
+        var c = ' marker-cluster-';
+        if (clusterSuitcases < 10) {
+          c += 'small';
+        } else if (clusterSuitcases < 100) {
+          c += 'medium';
+        } else {
+          c += 'large';
+        }
+
+        return new L.DivIcon({ html: '<div><span>' + clusterSuitcases + '</span></div>', className: 'marker-cluster' + c, iconSize: new L.Point(40, 40) });
+      },
+    });
+
+    // create each marker
+    for (var i = 0; i < markersInfo.length; i++) {
+      var markerInfo = markersInfo[i];
+
+      var marker = L.marker(
+        new L.LatLng(markerInfo["latitude"], markerInfo["longitude"]),
+        {
+          suitcases: markerInfo["suitcases"] || 0
+        }
+      );
+      marker.bindPopup(`<b> ${markerInfo["suitcases"]} suitcase(s) were donated in ${markerInfo["country"]} </b>`);
+      markers.addLayer(marker);
+    }
+
+    // add markers layer to map
+    map.addLayer(markers);
   });
 }
-
-function getColor(trips) {
-  if (trips === undefined) {
-    return 'transparent';
-  } else if (trips > 50) {
-    return '#0e7261';
-  } else if (trips > 40) {
-    return '#139f88';
-  } else if (trips > 30) {
-    return '#19cdaf';
-  } else if (trips > 20) {
-    return '#32e6c8';
-  } else if (trips > 10) {
-    return '#60ecd4';
-  } else {
-    return '#8df1e1';
-  }
-}
-
-function style(feature) {
-  return {
-    fillColor: getColor(feature.properties.trips),
-    weight: 2,
-    opacity: 1,
-    color: 'white',
-    dashArray: '3',
-    fillOpacity: 0.6
-  };
-}
-
-// highlight countries on mouseover event 
-function highlightFeature(e) {
-  var layer = e.target;
-
-  // set a thick grey border on the layer
-  layer.setStyle({
-    weight: 3,
-    color: '#666',
-    dashArray: '',
-    fillOpacity: 0.7
-  });
-
-  // bringing it to the front so that the border doesn’t clash with nearby states
-  // (but not for IE, Opera or Edge, since they have problems doing bringToFront on mouseover)
-  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-    layer.bringToFront();
-  }
-
-  // update info box
-  info.update(layer.feature.properties);
-}
-
-// unhighlight countries on mouseout event
-function resetHighlight(e) {
-  // reset the layer style to its default state (defined by our style function)
-  geojson.resetStyle(e.target);
-
-  // reset info box
-  info.update();
-}
-
-// zoom into the country on click
-function zoomToFeature(e) {
-  map.fitBounds(e.target.getBounds());
-}
-
-// add event handler on each country layers
-function onEachFeature(feature, layer) {
-  layer.on({
-    mouseover: highlightFeature,
-    mouseout: resetHighlight,
-    click: zoomToFeature
-  });
-}
-
-// create information box on the top right
-var info = L.control();
-
-info.onAdd = function (map) {
-  // create a div with a class "info"
-  this._div = L.DomUtil.create('div', 'info');
-  this.update();
-  return this._div;
-};
-
-// method that we will use to update the control based on feature properties passed
-info.update = function (props) {
-  const { trips, suitcases, weight, country } = props || {};
-  var infoHTML = '<h4>Total Donation</h4>';
-
-  if (!props) {
-    infoHTML += 'Hover over a country';
-  } else if (trips || suitcases || weight) {
-    infoHTML +=
-      '<b>' + country + '</b>' +
-      '<br />' + trips + (trips > 1 ? ' trips' : ' trip') +
-      '<br />' + suitcases + (suitcases > 1 ? ' suitcases' : ' suitcase') +
-      '<br />' + weight + (weight > 1 ? ' pounds' : ' pound');
-  } else {
-    infoHTML +=
-      '<b>' + country + '</b>' +
-      '<br /> No information';
-  }
-
-  this._div.innerHTML = infoHTML;
-}
-
-// add information box to map
-info.addTo(map);
-
-// create legend for colours
-var legend = L.control({ position: 'bottomright' });
-
-legend.onAdd = function (map) {
-  // create a div with a class "info" and "legend"
-  var div = L.DomUtil.create('div', 'info legend'),
-    grades = [1, 10, 20, 30, 40, 50],
-    labels = [];
-
-  // add title
-  div.innerHTML += '<h4> Number of Trips </h4>';
-
-  // loop through our density intervals and generate a label with a colored square for each interval
-  for (var i = 0; i < grades.length; i++) {
-    div.innerHTML +=
-      '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-      grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-  }
-  return div;
-};
-
-legend.addTo(map);
